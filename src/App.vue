55e9882829dec27def3324339a4fb5dad7e053cf
Alexandru Popescu <alexandrupopescu365@gmail.com>
2022-07-25 10:51:48 +0300

    first

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <BlockMe v-if="isPlaying" :delay="delay" @end="endGame"></BlockMe>
+  <ResultsShow v-if="showResults" :score="score" />
+</template>
+
+<script>
+import BlockMe from './components/BlockMe.vue'
+import ResultsShow from './components/ResultsShow.vue'
+export default {
+  name: 'App',
+  components: {
+    BlockMe,
+    ResultsShow
+   },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000   
+      this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
+  }  
+  }
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+</style>
